c0681c69d3bc0a36deba95caf5aa2a820e2b3816
kailous <kailous@live.cn>
2023-12-25 12:36:38 +0800

    优化主程序，添加了棋盘状态的输出，给行和列命名, 添加了标注。

--- js/script.js
+++ js/script.js
@@ -12,12 +12,14 @@ var playerNames = {
 };
 
 
+
 function createBoard() {
     for (var i = 0; i < columns * rows; i++) {
         var cell = document.createElement('div');
         cell.classList.add('cell');
         cell.addEventListener('click', dropPiece);
         board.appendChild(cell);
+        assignBingoId(board); // 给棋盘单元格分配binggo-id
     }
 }
 
@@ -38,11 +40,6 @@ function dropPiece() {
             piece.classList.add('piece', currentPlayer, 'animate-drop');
 
             var icon = document.createElement('iconpark-icon');
-            // 添加color属性，用于设置图标颜色
-            // icon.setAttribute('color', currentPlayer === 'userA' ? '#FF6666' : '#6B96FA');
-            // 添加stroke属性，用于设置图标描边颜色
-            // icon.setAttribute('stroke', currentPlayer === 'userA' ? '#fff' : '#fff');
-            // 添加stroke-width属性，用于设置图标描边宽度
             icon.setAttribute('stroke-width', '10');
             icon.setAttribute('name', 'Star');
             piece.appendChild(icon);
@@ -67,6 +64,11 @@ function dropPiece() {
         // 如果这一列已满，直接返回而不更改isDropping
         return;
     }
+    // 落子操作完成后，输出棋盘状态，如果已经输出过，刷新棋盘状态。
+    console.clear();
+    printBoard();
+    // 在控制台 输出当前落子位置
+    console.log(playerNames[currentPlayer] + '落子：' + this.getAttribute('columns') + ' - ' + this.getAttribute('rows'));
 }
 
 
@@ -125,6 +127,8 @@ function showVictoryPopup(player) {
     victoryPieceL.className = 'card-piece l ' + player;
     victoryPieceR.className = 'card-piece r ' + player;
     victoryPopup.style.display = 'block';
+    // 控制台输出胜利信息
+    console.log(playerNames[player] + ' 胜利!');
 }
 
 function closeVictoryPopup() {
@@ -142,6 +146,40 @@ function resetGame() {
     gameEnded = false;
     isDropping = false;
     closeVictoryPopup();
+    // 清空控制台
+    console.clear();
+}
+function printBoard() {
+    var boardState = [];
+    for (var row = 0; row < rows; row++) {
+        var rowState = [];
+        for (var col = 0; col < columns; col++) {
+            var cellIndex = row * columns + col;
+            var cell = board.children[cellIndex];
+            if (cell.hasChildNodes()) {
+                if (cell.firstChild.classList.contains('userA')) {
+                    rowState.push('A');
+                } else if (cell.firstChild.classList.contains('userB')) {
+                    rowState.push('B');
+                }
+            } else {
+                rowState.push('O');
+            }
+        }
+        boardState.push('|' + rowState.join('|') + '|');
+    }
+    console.log(boardState.join('\n'));
+}
+// 给棋盘单元格分配binggo-id
+function assignBingoId(board) {
+    var cells = board.getElementsByClassName('cell');
+    var columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G']; // 列号从A到G
+    for (var i = 0; i < cells.length; i++) {
+        var col = columns[i % 7]; // 使用字母表示列号
+        var row = Math.floor(i / 7) + 1; // 行号从1开始
+        cells[i].setAttribute('columns', col);
+        cells[i].setAttribute('rows', row);
+    }
 }
 
 createBoard();
